5b2848558f47876551cbacbd4dbaadfa44282da4
Patricio Inzaghi <patricio.inzaghi@gmail.com>
2020-03-11 11:41:29 +0100

    code refactor

--- examples/modsmartsync.c
+++ examples/modsmartsync.c
@@ -29,35 +29,34 @@ int func(int p, int n)
         stop_mbox = havoc(creg);
         stopdata_mbox = havoc(creg);
         
-        if(timeout){
-            if(nreg == creg){
-                nreg = creg+1;
-                round = STOP;
+        if(timeout && nreg == creg){
+            nreg = creg+1;
+            round = STOP;
 
-                cancelTimers();
-                msg* m = (msg *) malloc(sizeof(msg));
-                m->round = STOP;
-                m->reg = nreg;
-                m->timedout_msgs = timedout_messages();
-                send(all,m);
-            }
+            cancelTimers();
+            msg* m = (msg *) malloc(sizeof(msg));
+            m->round = STOP;
+            m->reg = nreg;
+            m->timedout_msgs = timedout_messages();
+            send(all,m);
+            
             continue; 
         }
 
-        if(count_change_regency(stop_mbox, creg+1)>n/3){
-            valid_timedout_msgs = valid_timedout_messages(stop_mbox);
-            if(nreg == creg){
-                nreg = creg+1;
-                round = STOP;
+        if(count_change_regency(stop_mbox, creg+1)>n/3 && nreg == creg){
+            
+            nreg = creg+1;
+            round = STOP;
 
-                cancelTimers();
-                msg* m = (msg *) malloc(sizeof(msg));
-                m->round = STOP;
-                m->reg = nreg;
-                m->timedout_msgs = valid_timedout_msgs;
-                send(all,m);
-            }
+            valid_timedout_msgs = valid_timedout_messages(stop_mbox);
             add_messages_to_order(valid_timedout_msgs);
+
+            cancelTimers();
+            msg* m = (msg *) malloc(sizeof(msg));
+            m->round = STOP;
+            m->reg = nreg;
+            m->timedout_msgs = valid_timedout_msgs;
+            send(all,m);
 
             continue;
         }
