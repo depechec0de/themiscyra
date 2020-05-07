08343bd9023a5891ea5458c2160c22f83f51fce9
Patricio Inzaghi <patricio.inzaghi@gmail.com>
2020-05-07 16:09:49 +0200

    refactor

--- examples/viewstamped.jumps.sync.c
+++ examples/viewstamped.jumps.sync.c
@@ -4,43 +4,130 @@ This file contains the (desired) sync version of viewstamped.jumps.c
 
 */
 
+
 INIT(){
     int view = 0;
     int vround = STARTVIEWCHANGE;
     int opnumber = 0;
     int nround = PREPARE;
+
+    int STARTVIEWCHANGE_mbox;
+    int DOVIEWCHANGE_mbox;
+    int PREPARE_mbox;
+    int PREPAREOK_mbox;
 }
 
-VIEWCHANGE(){
+MAIN(){
+    STARTVIEWCHANGE{
+        SEND(){
+            send(all, message(view,STARTVIEWCHANGE,p));
+        }
+        UPDATE(mbox){
+            STARTVIEWCHANGE_mbox = mbox;
+            if(count_messages(mbox) > f){
+                vround = DOVIEWCHANGE;
+            }
+        }
+    }        
+
+    DOVIEWCHANGE{
+        SEND(){
+            if( p!=primary(view,n)) && count_messages(STARTVIEWCHANGE_mbox) > f){
+                send(primary(view,n), message(view, DOVIEWCHANGE, p, log));
+            }
+        }
+        UPDATE(mbox){
+            if(p!=primary(view,n) && count_messages(STARTVIEWCHANGE_mbox) > f){
+                vround = STARTVIEW;
+            }
+            if(p==primary(view,n) && count_messages(mbox) > f){
+                vround = STARTVIEW;
+                DOVIEWCHANGE_mbox = mbox
+            }
+        }
+    }
+
+    STARTVIEW{
+        SEND(){
+            if( p==primary(view,n)) && count_messages(STARTVIEWCHANGE_mbox) > f) && count_messages(DOVIEWCHANGE_mbox) > f){
+                send(all, message(view,STARTVIEW,p));
+            }
+        }
+        UPDATE(mbox){
+            if( p==primary(view,n) ){
+                if(count_messages(STARTVIEWCHANGE_mbox) > f && count_messages(DOVIEWCHANGE_mbox) > f){
+                    computes_new_log();
+                    NORMALOP();
+                }
+            }else{
+                if(count_messages(STARTVIEWCHANGE_mbox) > f && count_messages(mbox) == 1){
+                    NORMALOP();
+                }
+            }
+        }
+    }        
 }
 
 NORMALOP(){
-
     PREPARE{
-        SEND(){
-            ...
-        }
 
+        SEND(){
+            if(p==primary(view,n)){
+                send(all, message(view,PREPARE,p));
+            }
+        }
         UPDATE(mbox){
-            ...
             if(jump_viewchange(mbox)){
-                VIEWCHANGE();
+                VIEWCHANGE(mbox);
+            }else{
+                if(!(count_messages(mbox) == 1 && mbox[0]->replica == primary(view,n))){
+                    out();
+                }else{
+                    PREPARE_mbox = mbox
+                }
             }
         }
     }
-
     PREPAREOK{
+
         SEND(){
-            ...
+            if(p!=primary(view,n) && count_messages(PREPARE_mbox) == 1){
+                send(primary(view,n), message(view,vround,opnumber,PREPAREOK,p));
+            }
+        }
+        UPDATE(mbox){
+            if(jump_viewchange(mbox)){
+                VIEWCHANGE(mbox);
+            }else{
+                if( p==primary(view,n) && count_messages(mbox) > f){
+                    PREPAREOK_mbox = mbox
+                }else{
+                    out();
+                }
+            }
+        }
+    }    
+
+    COMMIT{
+
+        SEND(){
+            if(p==primary(view,n) && count_messages(PREPAREOK_mbox) > f){
+                send(all, message(view,vround,opnumber,COMMIT,p));
+            }
         }
 
         UPDATE(mbox){
-            ...
             if(jump_viewchange(mbox)){
-                VIEWCHANGE();
+                VIEWCHANGE(mbox);
+            }else{
+                if(p!=primary(view,n) && count_messages(PREPARE_mbox) == 1 && count_messages(mbox) == 1){
+                    commit_to_log();
+                }else if(p==primary(view,n) && count_messages(PREPAREOK_mbox) > f){
+                    commit_to_log();
+                }else{
+                    out();
+                }
             }
         }
-    }
-
-
+    }       
 }
