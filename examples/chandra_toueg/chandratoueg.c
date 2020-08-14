0f0a52f5eb59f16dec1cc60f785b126c723fd50a
Patricio Inzaghi <patricio.inzaghi@gmail.com>
2020-08-14 14:11:07 +0200

    CT async with jumps

--- examples/chandra_toueg/chandratoueg.c
+++ examples/chandra_toueg/chandratoueg.c
@@ -35,9 +35,10 @@ struct msg* message(int phase, enum round_typ round, int estimate, int p, int ti
 _Bool value_decided(int p);
 // Inform the client of a decision, value_decided(p) := true
 _Bool decide(int estimate);
-
 // Look into the mbox for a phase >= `current_phase` with at least `size_to_count` messages in round `round`
-int get_phase_matching(struct list * mbox, int current_phase, enum round_typ round, int size_to_count){
+int phase_matching(struct list * mbox, int current_phase, enum round_typ round, int size_to_count);
+
+int phase_matching(struct list * mbox, int current_phase, enum round_typ round, int size_to_count){
     for(int i = current_phase; i <= max_phase_recv(mbox); i++){ 
         if(query(mbox, i, round) >= size_to_count){
             return i;
@@ -69,18 +70,24 @@ int main()
 
         mbox = havoc(phase, round);
 
-        if(!leader(phase) && !value_decided(p) && round == FIRST_ROUND){
-            
+        if(!leader(phase) && !value_decided(p) && round == FIRST_ROUND)
+        {
             send(message(phase, FIRST_ROUND, estimate, p, timestamp, null_bool()), leaderid(phase)); 
             round = SECOND_ROUND;
 
             continue;
         }
 
-        if(leader(phase) && get_phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2) >= phase && !value_decided(phase)){
-            
-            if(get_phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2) > phase){
-                phase = get_phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2);
+        
+        if  (leader(phase) && 
+            phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2) >= phase && 
+            // P -> Q iff (!P || Q), I jump or I am in the same message's timestamp
+            (phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2) != phase || round == FIRST_ROUND) && 
+            !value_decided(phase))
+        {
+            if(phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2) > phase)
+            {
+                phase = phase_matching(mbox, phase, FIRST_ROUND, (n-1)/2);
                 round = FIRST_ROUND;
             }
             
@@ -94,10 +101,15 @@ int main()
             continue;
         }
 
-        if(!leader(phase) && get_phase_matching(mbox, phase, SECOND_ROUND, 1) >= phase && !value_decided(phase)){
-
-            if(get_phase_matching(mbox, phase, SECOND_ROUND, 1) > phase){
-                phase = get_phase_matching(mbox, phase, SECOND_ROUND, 1);
+        if  (!leader(phase) && 
+            phase_matching(mbox, phase, SECOND_ROUND, 1) >= phase && 
+            // P -> Q iff (!P || Q), I jump or I am in the same message's timestamp
+            (phase_matching(mbox, phase, SECOND_ROUND, 1) != phase || round == SECOND_ROUND) && 
+            !value_decided(phase))
+        {
+            if(phase_matching(mbox, phase, SECOND_ROUND, 1) > phase)
+            {
+                phase = phase_matching(mbox, phase, SECOND_ROUND, 1);
                 round = SECOND_ROUND;
             }
 
@@ -112,10 +124,14 @@ int main()
             continue;
         }  
 
-        if(leader(phase) && get_phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2) >= phase && !value_decided(phase)){     
-            
-            if(get_phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2) > phase){
-                phase = get_phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2);
+        if  (leader(phase) && 
+            phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2)  >= phase && 
+            // P -> Q iff (!P || Q), I jump or I am in the same message's timestamp
+            (phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2) != phase || round == THIRD_ROUND) && !value_decided(phase))
+        {     
+            if(phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2) > phase)
+            {
+                phase = phase_matching(mbox, phase, THIRD_ROUND, (n-1)/2);
                 round = THIRD_ROUND;
             }
 
@@ -129,10 +145,15 @@ int main()
 
         }
 
-        if(!leader(phase) && get_phase_matching(mbox, phase, FOURTH_ROUND, 1) >= phase && !value_decided(phase)){
-
-            if(get_phase_matching(mbox, phase, FOURTH_ROUND, 1) > phase){
-                phase = get_phase_matching(mbox, phase, FOURTH_ROUND, 1);
+        if  (!leader(phase) && 
+            phase_matching(mbox, phase, FOURTH_ROUND, 1) >= phase && 
+            // P -> Q iff (!P || Q), I jump or I am in the same message's timestamp
+            (phase_matching(mbox, phase, FOURTH_ROUND, 1) != phase || round == FOURTH_ROUND) && 
+            !value_decided(phase))
+        {
+            if(phase_matching(mbox, phase, FOURTH_ROUND, 1) > phase)
+            {
+                phase = phase_matching(mbox, phase, FOURTH_ROUND, 1);
                 round = FOURTH_ROUND;
             }
 
@@ -146,16 +167,16 @@ int main()
             continue;
         }
 
-        if(!value_decided(p) && timeout(round)){
-            
+        if(!value_decided(p) && timeout(round))
+        {
             phase++;
             round = FIRST_ROUND;
 
             continue;
         } 
 
-        if(leader(phase) && value_decided(p)){
-            
+        if(leader(phase) && value_decided(p))
+        {
             round = FOURTH_ROUND;
             send(message(phase, FOURTH_ROUND, estimate, p, null_int(), true), to_all);
             phase++;
